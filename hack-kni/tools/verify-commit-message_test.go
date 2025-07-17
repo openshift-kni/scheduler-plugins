15324e3af01579e0b2eb3cd9c1361dae70f0829c
Shereen Haj <shajmakh@redhat.com>
2025-07-17 11:19:57 +0300

    TBC..
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>

--- hack-kni/tools/verify-commit-message_test.go
+++ hack-kni/tools/verify-commit-message_test.go
@@ -37,6 +37,50 @@ func TestRunWithOnlyNewlines(t *testing.T) {
 	}
 }
 
+func TestRunWithDifferentCommits(t *testing.T) {
+	testcases := []struct {
+		description      string
+		commitMessage    string
+		expectedExitCode int
+	}{
+		{
+			description: "valid KNI commit message",
+			commitMessage: `[KNI] doc about cascading **KNI SPECIFIC** cherry-picks
+
+We can avoid long chains of cherry-picked from commit
+references JUST AND ONLY for KNI-specific changes.
+
+Signed-off-by: Francesco Romani <fromani@redhat.com>`,
+			expectedExitCode: 0,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.description, func(t *testing.T) {
+			_, _, err := runCommand(t, tc.commitMessage)
+			if tc.expectedExitCode == 0 && err == nil {
+				// everything is as expected
+				return
+			}
+
+			if tc.expectedExitCode == 0 && err != nil {
+				t.Fatalf("Received unexpected error %v", err)
+			}
+
+			exiterr, ok := err.(*exec.ExitError)
+			if !ok {
+				t.Fatalf("Received unexpected error %T %v", err, err)
+			}
+
+			errCode := exiterr.ExitCode()
+			if errCode != tc.expectedExitCode {
+				t.Fatalf("Received unexpected exit code: expected %d got %d", tc.expectedExitCode, errCode)
+			}
+		})
+	}
+
+}
+
 func TestRunWithEmptyArgument(t *testing.T) {
 	_, errBuf, err := runCommand(t, "")
 	// run with empty commit message is unsupported and should fail
@@ -63,13 +107,15 @@ func TestRunWithoutArguments(t *testing.T) {
 	if !ok {
 		t.Fatalf("Received unexpected error %T %v", err, err)
 	}
+
 	errCode := exitErr.ExitCode()
 	if errCode != exitCodeErrorWrongArguments {
 		t.Fatalf("Received unexpected exit code %d", errCode)
 	}
+
 }
 
-// runCommand returns stdout as string, stderr as string, error code
+// runCommand returns stdout as string, stderr as string, error value
 func runCommand(t *testing.T, args ...string) (string, string, error) {
 	bin, err := getBinPath()
 	if err != nil {
