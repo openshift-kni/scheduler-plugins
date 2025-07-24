fe35539e58c488d6b38eba5aa5889924a0cd57af
Shereen Haj <shajmakh@redhat.com>
2025-07-24 11:33:51 +0300

    jul24
    Signed-off-by: Shereen Haj <shajmakh@redhat.com>

--- hack-kni/tools/verify-commit-message.go
+++ hack-kni/tools/verify-commit-message.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log"
 	"os"
+	"os/exec"
 	"strings"
 )
 
@@ -15,15 +16,22 @@ const (
 )
 
 const (
-	tagKNI = "[KNI]"
+	tagKNI      = "[KNI]"
+	tagUpstream = "[upstream]"
 
 	cherryPickLinePrefix = "(cherry picked from commit "
 	cherryPickLineSuffix = ")" // yes that simple
+
+	signedOffByPrefix = "Signed-off-by: "
+
+	konfluxUsername = "red-hat-konflux"
 )
 
 var (
-	errEmptyCommitMessage = errors.New("empty commit message")
-	errMissingTagKNI      = errors.New("missing tag: " + tagKNI)
+	errEmptyCommitMessage         = errors.New("empty commit message")
+	errMissingTagKNI              = errors.New("missing tag: " + tagKNI)
+	errMissingCherryPickReference = errors.New("missing cherry pick reference")
+	errWrongCherryPickReference   = errors.New("wrong cherry pick reference")
 )
 
 type commitMessage struct {
@@ -56,6 +64,10 @@ func (cm commitMessage) IsKNISpecific() bool {
 	return strings.Contains(cm.Summary(), tagKNI)
 }
 
+func (cm commitMessage) IsUpstream() bool {
+	return strings.Contains(cm.Summary(), tagUpstream)
+}
+
 // CherryPickOrigin returns the commit hash this commit was cherry-picked
 // from if this commit has cherry-pick reference; otherwise returns empty string.
 func (cm commitMessage) CherryPickOrigin() string {
@@ -74,13 +86,59 @@ func (cm commitMessage) CherryPickOrigin() string {
 	return "" // nothing found
 }
 
+func (cm commitMessage) IsKonflux() bool {
+	for idx := cm.NumLines() - 1; idx > 0; idx-- {
+		line := cm.lines[idx] // shortcut
+		signedOff, ok := strings.CutPrefix(line, signedOffByPrefix)
+		if !ok {
+			continue
+		}
+		if strings.HasPrefix(signedOff, konfluxUsername) {
+			return true
+		}
+	}
+	return false // nothing found
+}
 func verifyCommitMessage(commitMessage string) error {
 	cm := newCommitMessageFromString(commitMessage)
 	if cm.IsEmpty() {
 		return errEmptyCommitMessage
 	}
+	if cm.IsKonflux() {
+		return nil
+	}
+
 	if !cm.IsKNISpecific() {
 		return errMissingTagKNI
+	}
+
+	cpOrigin := cm.CherryPickOrigin()
+	upstream := cm.IsUpstream()
+
+	if cpOrigin != "" {
+		err := IsCommitInBranch(cpOrigin)
+		if err != nil {
+			return err
+		}
+	}
+
+	if upstream {
+		return errMissingCherryPickReference
+	}
+
+	return nil
+
+}
+
+func IsCommitInBranch(cpOrigin string) error {
+	cmd := exec.Command("git", "branch", "-r", "--contains", cpOrigin)
+	out, err := cmd.Output()
+	if err != nil {
+		return err
+	}
+	outStr := string(out)
+	if !strings.Contains(outStr, "upstream/master") { // TODO: Fix me; should be able to fetch the remote name
+		return errWrongCherryPickReference
 	}
 	return nil
 }
@@ -89,14 +147,14 @@ func main() {
 	if len(os.Args) != 2 {
 		programName := os.Args[0]
 		log.Printf("usage: %s wrong number of arguments expects: %s <commit-message>", programName, programName)
-		os.Exit(1)
+		os.Exit(exitCodeErrorWrongArguments)
 	}
 
 	err := verifyCommitMessage(os.Args[1])
 	if err != nil {
 		log.Printf("verification failed: %v", err)
-		os.Exit(2)
+		os.Exit(exitCodeErrorVerificationFailed)
 	}
 
-	os.Exit(0) // all good! redundant but let's be explicit about our success
+	os.Exit(exitCodeSuccess) // all good! redundant but let's be explicit about our success
 }
